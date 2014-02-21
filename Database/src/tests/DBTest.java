87fcdf09f0149e51071c229bb50e89c80d8f5613
Joël Joly <joel.joly@gmail.com>
2014-02-21 09:51:13 +0100

    Open connection fixture

--- Database/src/tests/DBTest.java
+++ Database/src/tests/DBTest.java
@@ -4,7 +4,9 @@ import static org.junit.Assert.*;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
 
 import utils.AccessBD;
@@ -12,13 +14,27 @@ import utils.AccessBD;
 
 public class DBTest {
 
+	Connection connection;
+	 
+	@Before
+	public void setUp() throws Exception {
+		connection = AccessBD.getConnection("../BD/bd.mdb");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		if (connection != null) {
+			connection.close();
+			connection = null;
+		}
+	}
+	
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 	}
 
 	@Test
 	public void testGetConnection() throws SQLException {
-		Connection connection = AccessBD.getConnection("../BD/bd.mdb");
 		assertNotNull(connection);
 	}
 
